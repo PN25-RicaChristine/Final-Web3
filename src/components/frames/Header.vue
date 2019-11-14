bb608244850fba643dfb695d4c2b00ae104a36a2
Gwapa Ako Kunoh <nylael.nilue18181919@gmail.com>
2019-11-14 20:53:34 +0800

    First Commit

--- src/components/frames/Header.vue
+++ src/components/frames/Header.vue
@@ -6,71 +6,144 @@
       <v-toolbar-title>SpotMe</v-toolbar-title>
 
       <v-spacer></v-spacer>
+      <!--Login modal-->
+      <div class="text-center">
+        <v-dialog v-model="login" width="600" height="60">
+          <template v-slot:activator="{ on }">
+            <v-btn color="warning" id="signbtn" v-on="on">Login</v-btn>
+          </template>
 
-      <v-btn color="error" class="mr-4" id="loginbtn">Login</v-btn>
+          <v-card>
+            <v-flex>
+              <v-card-title text-align="center" class="headline grey lighten-2">Login</v-card-title>
+            </v-flex>
+            <v-app id="inspire">
+              <v-col cols="50">
+                <v-card-text>
+                  <v-form>
+                    <v-text-field
+                      label="Username"
+                      name="login"
+                      type="text"
+                      :prepend-icon="'mdi-account'"
+                    >
+                      <v-icon>mdi-pin</v-icon>
+                    </v-text-field>
 
-      <v-btn color="warning" id="signbtn">Sign Up</v-btn>
+                    <v-text-field
+                      id="password"
+                      label="Password"
+                      name="password"
+                      type="password"
+                      :prepend-icon="'mdi-key-variant'"
+                    />
+                  </v-form>
+                </v-card-text>`
+                <v-card-actions>
+                  <v-spacer/>
+                  <v-flex>
+                    <v-btn color="primary">Login</v-btn>
+                  </v-flex>
+                </v-card-actions>
+              </v-col>
+            </v-app>
+          </v-card>
+        </v-dialog>
+      </div>
+      <!--Sign Up modal-->
+      <div class="text-center">
+        <v-dialog v-model="signup" width="600" height="60">
+          <template v-slot:activator="{ on }">
+            <v-btn color="warning" id="signbtn" v-on="on">Sign Up</v-btn>
+          </template>
+
+          <v-card>
+            <v-flex>
+              <v-card-title text-align="center" class="headline grey lighten-2">Sign Up</v-card-title>
+            </v-flex>
+            <v-app id="inspire">
+              <v-col cols="50">
+                <v-card-text>
+                  <v-form>
+                    <v-text-field
+                      label="Name"
+                      name="name"
+                      type="text"
+                      :prepend-icon="'mdi-account'"
+                    >
+                      <v-icon>mdi-pin</v-icon>
+                    </v-text-field>
+                    <v-text-field
+                      label="Username"
+                      name="uname"
+                      type="text"
+                      :prepend-icon="'mdi-account'"
+                    >
+                      <v-icon>mdi-pin</v-icon>
+                    </v-text-field>
+                    <v-text-field
+                      label="Address"
+                      name="login"
+                      type="text"
+                      :prepend-icon="'mdi-map-marker'"
+                    >
+                      <v-icon>mdi-pin</v-icon>
+                    </v-text-field>
+                    <v-text-field
+                      label="Email"
+                      name="email"
+                      type="email"
+                      :prepend-icon="'mdi-mail'"
+                    >
+                      <v-icon>mdi-pin</v-icon>
+                    </v-text-field>
+
+                    <v-text-field
+                      id="password"
+                      label="Password"
+                      name="password"
+                      type="password"
+                      :prepend-icon="'mdi-key-variant'"
+                    />
+                     <v-text-field
+                      id="password"
+                      label="Confirm Password"
+                      name="password"
+                      type="password"
+                      :prepend-icon="'mdi-key-variant'"
+                    />
+                  </v-form>
+                </v-card-text>`
+                <v-card-actions>
+                  <v-spacer/>
+                  <v-flex>
+                    <v-btn color="primary">Register</v-btn>
+                  </v-flex>
+                </v-card-actions>
+              </v-col>
+            </v-app>
+          </v-card>
+        </v-dialog>
+      </div>
     </v-app-bar>
-
-    <v-app id="inspire">
-    <v-content>
-      <v-container
-        class="fill-height"
-        fluid
-      >
-        <v-row
-          align="center"
-          justify="center"
-        >
-          <v-col
-            cols="12"
-            sm="8"
-            md="4"
-          >
-            <v-card class="elevation-12">
-              <v-toolbar
-                color="primary"
-                dark
-                flat
-              >
-                <v-toolbar-title>Login</v-toolbar-title>
-                <v-spacer />
-              </v-toolbar>
-              <v-card-text>
-                <v-form>
-                  <v-text-field
-                    label="Username"
-                    name="login"
-                    type="text"
-                    :prepend-icon="'mdi-account'"
-                  ><v-icon>mdi-pin</v-icon></v-text-field>
-                
-
-                  <v-text-field
-                    id="password"
-                    label="Password"
-                    name="password"
-                    type="password"
-                  />
-                </v-form>
-              </v-card-text>
-              <v-card-actions>
-                <v-spacer />
-                <v-btn color="primary">Login</v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-content>
-  </v-app>
   </div>
 </template>
 <style scoped>
-#loginbtn{
-    border-radius: 50px !important;
+#loginbtn {
+  border-radius: 50px !important;
 }
-#signbtn{
-    border-radius: 50px !important;
+#signbtn {
+  border-radius: 50px !important;
 }
 </style>
+
+<script>
+export default {
+  data() {
+    return {
+      login: false,
+      signup: false
+    };
+  }
+};
+</script>
